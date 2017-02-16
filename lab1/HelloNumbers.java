af53c0b6f15eb4bec88f6f71a74791fc47e48b4d
Miranda Zhang <mirandazmq@gmail.com>
2017-02-16 12:29:03 -0800

    Fully Completed lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+		int x = 0;
+		int sum = 0;
+        while (x < 10) {
+            System.out.println(sum);
+            x = x + 1;
+            sum = sum + x;
+        }
+	}
+}
